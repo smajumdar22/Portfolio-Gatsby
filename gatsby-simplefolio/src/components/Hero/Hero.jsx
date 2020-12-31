1c478e89b47342bba9c8e491bfbfb509782fc283
smajumdar22 <smajumdar22@gmail.com>
2020-12-30 19:25:11 -0800

    Update url

--- gatsby-simplefolio/src/components/Hero/Hero.jsx
+++ gatsby-simplefolio/src/components/Hero/Hero.jsx
@@ -29,7 +29,7 @@ const Header = () => {
             {title || 'Hi, my name is'}{' '}
             <span className="text-color-main">{name || 'Shubham Majumdar'}</span>
             <br />
-            {subtitle || "I'm a Fullstack Developer"}
+            {subtitle || "Software Engineer | Fullstack Developer"}
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
